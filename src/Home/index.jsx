269b7870afa587bdaf4db9876c8558ac7a16b098
Rahulsunda1 <rahulsunda977@gmail.com>
2024-12-10 23:01:49 +0530

    correct1

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,4 +1,4 @@
-import Header from '@/components/custom/Header'
+import Header from './../components/Custom/Header'
 import { UserButton } from '@clerk/clerk-react'
 import { AtomIcon, Edit, Share2 } from 'lucide-react'
 import React from 'react'
