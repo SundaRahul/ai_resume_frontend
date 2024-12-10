6f006191e7087bacc6718b7410ffdbc65c9791af
Rahulsunda1 <rahulsunda977@gmail.com>
2024-12-11 00:01:29 +0530

    c6

--- src/my-resume/[Resume_id]/view/index.jsx
+++ src/my-resume/[Resume_id]/view/index.jsx
@@ -9,7 +9,7 @@ import PreviewSection from './../../../Dashboard/Resume/components/PreviewSectio
 
 function ViewResume() {
 
-    const [resumeInfo,setResumeInfo]=useState('na');
+    const [resumeInfo,setResumeInfo]=useState([]);
     const { Resume_id } = useParams();
 
     useEffect(()=>{
